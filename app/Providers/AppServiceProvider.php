2402c1e343a17fa245b6d572c83d43c907d5edd9
brofatkur <fatkursukses@gmail.com>
2026-08-02 16:33:12 +0800

    Guarantee setting() helper loading via AppServiceProvider register method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $helperPath = app_path('Helpers/helpers.php');
+        if (file_exists($helperPath)) {
+            require_once $helperPath;
+        }
     }
 
     /**
